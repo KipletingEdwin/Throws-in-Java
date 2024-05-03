5d6bb2272adf106f6d3499116d941582bb1742f6
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-05-03 09:33:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public  static double divide(double a, double b) throws  ArithmeticException{
+        if(b == 0){
+            throw  new ArithmeticException("Invalid Division");
+        } else {
+            return  a/b;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(divide(10, 0));
+        } catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
